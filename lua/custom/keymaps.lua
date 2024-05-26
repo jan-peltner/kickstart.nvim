37db0665a505c6bd8aa800686d5009e9f73fab7f
jan <jan@netshake.com>
2024-05-26 20:34:04 +0200

    add buff navigation keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,7 +1,11 @@
 -- #Neotree toggle
 vim.keymap.set('n', '<leader>e', '<CMD>Neotree toggle=true<CR>', { desc = 'Toggle Neotree' })
+-- #Navigate buffers
+-- ##Focus next buffer
+vim.keymap.set('n', 'L', '<CMD>bn<CR>', { desc = 'Toggle Neotree' })
+-- ##Focus previous buffer
+vim.keymap.set('n', 'H', '<CMD>bp<CR>', { desc = 'Toggle Neotree' })
 -- #Delete buffers
---
 -- ##Delete current buffer
 vim.keymap.set('n', '<leader>bd', function()
   vim.api.nvim_buf_delete(0, { force = true })
