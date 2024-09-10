9e72c86b13fd00419e0c51347b1c99af44a3d658
jan <jan.peltner@ggmgastro.com>
2024-09-10 13:46:37 +0200

    add carbon-now plugin

--- init.lua
+++ init.lua
@@ -153,7 +153,6 @@ require('lazy').setup({
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
-
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
---
+++ lua/custom/plugins/carbon-now.lua
@@ -0,0 +1,11 @@
+return {
+  'ellisonleao/carbon-now.nvim',
+  lazy = true,
+  cmd = 'CarbonNow',
+  opts = {
+    options = {
+      titlebar = '',
+      theme = 'night-owl',
+    },
+  },
+}
